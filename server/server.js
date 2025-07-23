e1df3f52d497d2c1e6e4b188ba9739dc34355af8
Mousam Marandi <145921013+Mousam24@users.noreply.github.com>
2025-07-23 23:38:23 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/flashcardDB", {
+mongoose.connect(process.env.MONGO_URI ,{
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log("✅ MongoDB Connected"))
